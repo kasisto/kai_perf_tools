74c03f7f11f2f7109bae2779f39263f442e09315
Martin John Lopez <martinjohnlopez@Martins-MacBook-Pro.local>
2021-08-31 17:41:37 +0800

    update script

--- scripts/perf_helper.py
+++ scripts/perf_helper.py
@@ -1,10 +1,17 @@
-import argparse, json, requests
+import argparse, json, os, requests
 from models.cms_doc import Assistant, CmsDoc
 from models.publishing import PublishingObj, PublishingPayload
 
 class PerformanceHelpers:
-    def __init__(self, path):
-        self.url = url
+    def __init__(self):
+        self.url = os.environ.get('URL')
+        self.headers = {
+            'secret': os.environ.get('ASSISTANT_SECRET'),
+            'assistant_name': os.environ.get('ASSISTANT_NAME')
+        }
+        self.application_headers = {
+            'secret': os.environ.get('APPLICATION_SECRET')
+        }
 
     def post(self, path, data, headers, auth):
         full_path = self.url + path
@@ -24,44 +31,34 @@ class PerformanceHelpers:
         Ensure that the global segment is published to the target
         """
         path = '/kai/api/v1/content/segments'
-        headers = {
-            'secret': 'kaidemo-en-us-2',
-            'assistant_name': 'kaidemo-en-us'
-        }
 
-        res = self.post(path, CmsDoc(f'global').get_json(), headers, ('automated_crud_test', 'pw'))
-        rev = self.get_latest_revision(f'global', headers, ('automated_crud_test', 'pw'))['revision_id']
+        res = self.post(path, CmsDoc(f'global').get_json(), self.headers, ('automated_crud_test', 'pw'))
+        rev = self.get_latest_revision(f'global', self.headers, ('automated_crud_test', 'pw'))['revision_id']
         pay = PublishingPayload(PublishingObj('segment', f'global', rev).get_json()).get_json()
 
-        pub = self.publish(pay, headers, ('automated_crud_test', 'pw'))
+        pub = self.publish(pay, self.headers, ('automated_crud_test', 'pw'))
 
 
     def create_assistant_versions(self, n):
-        path = 'https://kaidemo-en-us-qa.kitsys.net:8090/kai/api/v1/content/segments'
-        publishing_path = 'https://kaidemo-en-us-qa.kitsys.net:8090/kai/api/v1/publishing/stage/documents'
-        headers = {
-            'secret': 'kaidemo-en-us-2',
-            'assistant_name': 'kaidemo-en-us'
-        }
+        path = self.url + '/kai/api/v1/content/segments'
+        publishing_path = self.url + '/kai/api/v1/publishing/stage/documents'
 
         for x in range(n):
             path = '/kai/api/v1/content/segments'
-            res = self.post(path, CmsDoc(f'test_{x}').get_json(), headers, ('automated_crud_test', 'pw'))
-            rev = self.get_latest_revision(f'test_{x}', headers, ('automated_crud_test', 'pw'))['revision_id']
+            res = self.post(path, CmsDoc(f'test_{x}').get_json(), self.headers, ('automated_crud_test', 'pw'))
+            rev = self.get_latest_revision(f'test_{x}', self.headers, ('automated_crud_test', 'pw'))['revision_id']
             pay = PublishingPayload(PublishingObj('segment', f'test_x', rev).get_json()).get_json()
 
-            pub = self.publish(pay, headers, ('automated_crud_test', 'pw'))
+            pub = self.publish(pay, self.headers, ('automated_crud_test', 'pw'))
             print(pub.json())
 
 
     def create_assistants(self, n):
         path = '/kai/api/v1/content/assistants'
-        headers = {
-            'secret': 'ef576554-637f-11e8-adc0-fa7ae01bbebc'
-        }
+
         for x in range(n):
             assistant = Assistant(f'test_{x}').get_json()
-            res = self.post(path, assistant, headers, ('automated_crud_test', 'pw'))
+            res = self.post(path, assistant, self.application_headers, ('automated_crud_test', 'pw'))
             print(res.json())
 
 
@@ -70,8 +67,8 @@ if __name__ == '__main__':
     parser.add_argument('--assistant')
     parser.add_argument('--n', type=int)
     args = parser.parse_args()
-    url = 'https://kaidemo-en-us-qa.kitsys.net:8090'
-    perf_helper = PerformanceHelpers(url)
+    url = 'https://ncr-mt-en-us-qa.kitsys.net:8090'
+    perf_helper = PerformanceHelpers()
     perf_helper.ensure_global_segment_is_published()
 
     if args.assistant == 'True':
