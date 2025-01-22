779f5632d3d831ca0147bf68abc6fefc7dee4206
gerard-kai <113816006+gerard-kai@users.noreply.github.com>
2025-01-22 08:14:41 -0500

    update origin path

--- capi_jmeter/scripts/upload_report.py
+++ capi_jmeter/scripts/upload_report.py
@@ -57,6 +57,6 @@ def upload_report(folder_name, origin_folder_path, bucket):
         logging.error(f"Error uploading folder {folder_name}: {e}")
 
 folder_name = 'html'
-origin_folder_path = os.path.abspath(f"capi_jmeter/configs/kcb/reports")
+origin_folder_path = "../configs/kcb/reports/html/"
 bucket = 'kasisto-customer-data-qa'
 upload_report(folder_name, origin_folder_path, bucket)
